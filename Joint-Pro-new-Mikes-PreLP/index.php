1e2fe441dfd01c09912bed052e18a1e84fe9ea4b
voiceferate <vanyavi06@gmail.com>
2021-05-28 18:22:37 +0300

    add gtm

---
+++ Joint-Pro-new-Mikes-PreLP/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="robots" content="noindex, nofollow">
+  <meta name="robots" content="noimageindex">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" type="image/png" href="imgs/favicon.png" />
+  <meta name="format-detection" content="telephone=no">
+
+  <title>Set Proper Title</title>
+
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="prerender" href="https://activehealthco.com/Sash/preloading-links-test/usual-loading/JointPro_LP_2021/index.php">
+
+
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600;700;800&display=swap" rel="stylesheet preload" as="font">
+  
+  <style>
+    body,html{margin:0;padding:0}body{font-family:"Open Sans",sans-serif;font-size:16px;font-weight:400;position:relative;letter-spacing:normal;color:#000;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale;-webkit-text-size-adjust:100%;-moz-text-size-adjust:100%;-ms-text-size-adjust:100%;text-size-adjust:100%;text-rendering:optimizeLegibility}a,b,body,div,footer,h1,h2,h3,h4,h5,h6,header,input,label,li,main,ol,p,section,span,strong,u,ul{box-sizing:border-box;margin:0;padding:0;-webkit-box-sizing:border-box;-moz-box-sizing:border-box}.cr{color:#dc0000;font-weight:800}.cb{color:#003078;font-weight:800}.main{min-height:100vh;display:flex;align-items:center;justify-content:center;background:#ffe6d4;background-size:cover;padding:20px 20px 40px}.content{max-width:910px;padding:30px 0;width:100%;background-color:#fff;border-radius:10px;text-align:center}.content-wrapper{padding:0 150px}.content_headline{font-weight:700;font-size:33px;line-height:1.16;margin:0 auto}.content_headline-second{margin:33px auto;font-size:26px}.content_img{max-width:561px;margin:0 auto 40px;cursor:pointer}.content_img picture img{width:100%;height:auto}.content_img a{position:relative;display:block}.content_img-btn{position:absolute;top:30px;right:-40px;display:flex;align-items:center;justify-content:space-between;padding:14px 10px;border:1px solid #000;background-color:rgba(0,0,0,.6);border-radius:9px;color:#fff;text-decoration:none;font-size:14px}.content_img-btn span{padding:0 10px}.content_cta{display:block;cursor:pointer;max-width:700px;width:100%;margin:40px auto;padding:10px 20px;background-color:#ffc600;box-shadow:inset 0 -3px 2.91px .09px rgba(145,92,17,.75);font-size:22px;line-height:1.667;color:#000;text-decoration:none;font-weight:800;transition:box-shadow .3s ease-in;text-transform:uppercase;letter-spacing:-.02em}.content_cta:hover{box-shadow:inset 0 -1px 1.91px .09px rgba(145,92,17,.75)}.content_text{text-align:justify;font-size:16px;line-height:1.44;font-weight:600;max-width:560px;margin:0 auto}@media screen and (max-width:900px){.content{padding:30px 30px}.content-wrapper{padding:0}.content_img-btn{right:30px}.content_headline{font-size:4vw}.content_headline br{display:none}.main-left__JFR{margin-left:0}}@media screen and (max-width:900px){.main-left__JFR{width:100%}}@media screen and (max-width:420px){.content{padding:30px 20px}.main-left__JFR--header{font-size:4vw}.main-left__JFR--info span{font-size:11px}.content_headline{font-size:5.6vw}.content_headline-second{margin:1em;font-size:4.6vw}.content_cta{font-size:5.5vw;width:90%;padding:15px 20px;line-height:1.467}.content_img{margin:30px auto}.content_img-btn{font-size:11px;padding:11px 7px;top:10px;right:10px}.content_img-btn img{margin-right:3px}.content_img-btn span{padding:0 8px}}@media screen and (max-width:375px){.content_cta{font-size:4.5vw}}
+  </style>
+
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5LGMQRG');</script>
+<!-- End Google Tag Manager -->
+
+</head>
+
+<body>
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5LGMQRG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+  <main class="main">
+    <div class="content">
+
+      <h1 class="content_headline">Natural <span class="cr">7-Second</span> Daily Ritual<br>
+        Restores <span class="cr">Joint Health</span> On Auto-Pilot</h1>
+      <h2 class="content_headline content_headline-second">(Watch Video)</h2>
+
+    <div class="content-wrapper">
+
+      <div class="content_img">
+        <a class="" href="https://activehealthco.com/Sash/preloading-links-test/usual-loading/JointPro_LP_2021/index.php">
+          <picture>
+            <source srcset="imgs/video_mob.jpg" media="(max-width: 420px)">
+            <img src="imgs/video.jpg" width="561" height="360" alt="video playscreen">
+          </picture>
+          <div class="content_img-btn"><img src="imgs/sound.png" alt=""><span>Click for sound</span></div>
+        </a>
+      </div>
+
+      <a class="content_cta" href="https://activehealthco.com/Sash/preloading-links-test/usual-loading/JointPro_LP_2021/index.php">Click Here To Watch The&nbsp;Video</a>
+
+      <p class="content_text">Scientists and doctors from Duke University, Cornell Medical, and the
+      University of Chicago confirmed a simple 7 second daily ritual repair joints without injections or surgery...<br>
+      If you or anyone you know is struggling with joint pain please watch this video presentation now before it's taken down...</p>
+    </div>
+    </div>
+  </main>
+
+  <script>
+
+  async function track() {
+    let response = await fetch('https://order.exportgroup.eu/tracking/', {
+      method: 'GET',
+      mode: "no-cors"
+    });
+    if (response.ok) { // если HTTP-статус в диапазоне 200-299
+      // получаем тело ответа (см. про этот метод ниже)
+      let json = await response.json();
+      console.log(json)
+    } else {
+      console.log(response);
+    }
+  }
+
+  
+  setTimeout(track, 1000)
+
+
+  </script>
+
+
+</body>
+
+</html>
